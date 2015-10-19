48bbf7b2b8e7315ba7957f1c11356b53550aae4c
Your Name <you@example.com>
2015-10-19 11:45:19 +0200

    lab2_1

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -3,9 +3,9 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 var ec2 = new AWS.EC2();
 
-AWS.config.update({region: 'us-west-2'});
+//AWS.config.update({region: 'us-west-2'});
 var task =  function(request, callback){
-	
+/*	
 var params = {
   DryRun: false,
  // Filters: [],
@@ -16,7 +16,25 @@ var params = {
 ec2.describeInstances(null, function(err, data) {
 	if (err) callback(null, "Error  " + err + err.stack); // an error occurred
 	else     callback(null, data);    // successful response
+});*/
+
+var params = {
+  ImageId: 'ami-fc2fa2cc', /* required */
+  MaxCount: 1, /* required */
+  MinCount: 1, /* required */
+  AdditionalInfo: 'created automatically',
+  ClientToken: 'yyy',
+  InstanceInitiatedShutdownBehavior: 'terminate',
+  InstanceType: 't1.micro',
+  Monitoring: {
+    Enabled: false /* required */
+  },
+};
+ec2.runInstances(params, function(err, data) {
+	if (err) callback(null, "Error  " + err + err.stack); // an error occurred
+	else     callback(null,  data.Instances[0].NetworkInterfaces   + '\r\n ' + data);    // successful response
 });
+
 	
 }
 
