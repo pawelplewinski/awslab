f33d66cda567d96fb6d80d05854ae2d4812cee6e
Your Name <you@example.com>
2015-10-19 10:56:14 +0200

    lab2_1

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -5,7 +5,7 @@ AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
 	
-ec2.describeInstances(params, function(err, data) {
+ec2.describeInstances({}, function(err, data) {
 	if (err) callback(null, "Error"); // an error occurred
 	else     callback(null, data);    // successful response
 });
