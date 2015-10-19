5a91ec5f28a9f22145ca93df8b39d34ec30786a1
Your Name <you@example.com>
2015-10-19 10:41:34 +0200

    lab2_1

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,10 +1,14 @@
 //stub for lab 1_2
 var AWS = require('aws-sdk');
-
+var ec2 = new AWS.EC2();
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
 	
+ec2.describeInstances(params, function(err, data) {
+	if (err) callback("Error"); // an error occurred
+	else     callback(data);    // successful response
+});
 	
 }
 
